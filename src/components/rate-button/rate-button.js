d5aa803c3388353df7ea5def8b214b35773dccb7
Your Name <your_email@whatever.com>
2020-08-15 15:54:17 +0500

    Add component rate-button

---
+++ src/components/rate-button/rate-button.js
@@ -0,0 +1,71 @@
+import "./rate-button.scss";
+
+class RateButton {
+  constructor(component) {
+    this.component = component;
+    this.count = this.component.dataset.count;
+    this.input = this.component.querySelector("input");
+    this._attachEventHandlers();
+  }
+
+  _attachEventHandlers() {
+    this.component &&
+      this.component.addEventListener("click", this._toggleCount);
+  }
+
+  _toggleCount = (event) => {
+    let el = (event || {}).target;
+    if (!el || !this.input) {
+      return null;
+    }
+    if (el.tagName === "IMG") {
+      el = el.closest("div.rate-button__rate");
+    }
+    let { rate, index } = el.dataset || {};
+    rate = parseFloat(rate);
+    index = parseFloat(index);
+    if (!rate) {
+      this.input.value = index;
+      for (let i = 1; i <= index; i++) {
+        const prev = this.component.querySelector(`[data-index="${i}"]`);
+        prev.dataset.rate = 1;
+        const classList = prev.classList;
+        if ([].indexOf.call(classList, "rate-button__rate_checked") === -1) {
+          prev.classList.add("rate-button__rate_checked");
+        }
+      }
+    } else if (rate && index) {
+      this.input.value = index - 1;
+      el.dataset.rate = 0;
+      el.classList.remove("rate-button__rate_checked");
+      for (let i = this.count; i >= index; i--) {
+        const prev = this.component.querySelector(`[data-index="${i}"]`);
+        prev.dataset.rate = 0;
+        const classList = prev.classList;
+        if ([].indexOf.call(classList, "rate-button__rate_checked") > -1) {
+          prev.classList.remove("rate-button__rate_checked");
+        }
+      }
+    }
+  };
+}
+
+export default function renderComponent(callbackWhenInitialized) {
+  (() => {
+    const buttons = [].map.call(
+      document.querySelectorAll(".rate-button__button"),
+      (node) => {
+        return new RateButton(node);
+      }
+    );
+
+    if (
+      callbackWhenInitialized &&
+      typeof callbackWhenInitialized === "function"
+    ) {
+      callbackWhenInitialized(buttons);
+    }
+  })();
+}
+
+renderComponent();
