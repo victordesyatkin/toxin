f2c67bad095a85de881b45a5b04f76d73c8bca5e
Victor Desyatkin <vitek1985@mail.ru>
2020-09-04 01:15:07 +0500

    Add focus/hover button/input

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,1 +1,15 @@
+import upperFirst from "lodash/upperFirst";
+import toLower from "lodash/toLower";
+
 import "./input.scss";
+
+(function () {
+  $($(".input")).each(function () {
+    const _handlerIn = () => {
+      $(this).toggleClass("input_hover-focus");
+    };
+    const _button = $("button", this);
+    const _handlerOut = _handlerIn;
+    $(_button).hover(_handlerIn, _handlerOut);
+  });
+})();
