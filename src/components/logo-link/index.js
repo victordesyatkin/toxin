c7aa4a573aeaa6327c3d1a899facc15d50987014
Victor Desyatkin <vitek1985@mail.ru>
2021-01-18 00:43:21 +0500

    проверить код по стайлгайду ... #5(logo-image add modificator type (color, monochromatic), add logo-link (single responsibility principle))

---
+++ src/components/logo-link/index.js
@@ -0,0 +1,3 @@
+import "./logo-image.scss";
+
+import "./logo-link.scss";
--- src/pages/colors-and-type/colors-and-type.js
+++ src/pages/colors-and-type/colors-and-type.js
@@ -1,5 +1,5 @@
-import "../demo-base/demo-base";
 import "../../components/font-sample";
 import "../../components/color-sample";
+import "../demo-base/demo-base";
 
 import "./colors-and-type.scss";
