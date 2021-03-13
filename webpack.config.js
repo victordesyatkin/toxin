7f76784567875475ae65486e64168d34ced24f05
Victor Desyatkin <vitek1985@mail.ru>
2021-03-14 02:04:07 +0500

    fix() colors-and-type bp 2.13

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const StylelintPlugin = require('stylelint-webpack-plugin');
 const path = require("path");
 const fs = require("fs");
 const webpack = require("webpack");
@@ -67,6 +68,12 @@ module.exports = (env = {}) => {
         jQuery: 'jquery',
       }),
       ...htmlPlugins,
+      new StylelintPlugin({
+        configFile: path.resolve(__dirname, '.stylelintrc.json'),
+        context: path.resolve(__dirname),
+        files: 'src/**/*.(s(c|a)ss|css)',
+        fix: true
+      }),
       new webpack.HotModuleReplacementPlugin(),
     ];
     if (isProduction) {
