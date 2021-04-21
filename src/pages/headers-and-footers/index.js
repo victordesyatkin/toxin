837514aaeb5e6f0610b02c6ea41f5b6196051c40
Victor Desyatkin <vitek1985@mail.ru>
2021-04-21 23:34:16 +0500

    fix(): transferred modifier, etc #5

--- src/pages/headers-and-footers/index.js
+++ src/pages/headers-and-footers/index.js
@@ -2,4 +2,8 @@ import { makeComponentLoadHandler } from '../../helpers';
 import HeadersAndFooters from './HeadersAndFooters';
 import data from './data.json';
 
-$(window).on('load', { props: data }, makeComponentLoadHandler(HeadersAndFooters));
+$(window).on(
+  'load',
+  { props: data },
+  makeComponentLoadHandler(HeadersAndFooters)
+);
